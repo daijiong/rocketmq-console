f57b68f285c744bd66c00e4fc9809e70f9b40c43
daijiong <daijiong@successhr.com>
2022-07-06 21:39:05 +0800

    去除spring-boot-admin

--- src/main/java/org/apache/rocketmq/console/config/MetricsConfig.java
+++ src/main/java/org/apache/rocketmq/console/config/MetricsConfig.java
@@ -1,23 +1,23 @@
-package org.apache.rocketmq.console.config;
-
-import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 监控指标配置
- *
- * @author daijiong
- * @version : MethricsConfig.java, v 0.1 2021/3/24 15:13 daijiong Exp $$
- */
-@Configuration
-public class MetricsConfig {
-
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(
-            @Value("${spring.application.name}") String applicationName) {
-        return (registry) -> registry.config().commonTags("application", applicationName);
-    }
-}
+//package org.apache.rocketmq.console.config;
+//
+//import io.micrometer.core.instrument.MeterRegistry;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 监控指标配置
+// *
+// * @author daijiong
+// * @version : MethricsConfig.java, v 0.1 2021/3/24 15:13 daijiong Exp $$
+// */
+//@Configuration
+//public class MetricsConfig {
+//
+//    @Bean
+//    MeterRegistryCustomizer<MeterRegistry> configurer(
+//            @Value("${spring.application.name}") String applicationName) {
+//        return (registry) -> registry.config().commonTags("application", applicationName);
+//    }
+//}
